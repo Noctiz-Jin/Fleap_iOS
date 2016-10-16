8879a9ba7a292324c38554beb716b5706236679f
Noctiz <noctizgame@gmail.com>
2016-10-15 21:42:36 -0700

    [Pod Integration] pod init pod install

---
+++ Pods/Target Support Files/Pods-FleapTests/Pods-FleapTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FleapTests : NSObject
+@end
+@implementation PodsDummy_Pods_FleapTests
+@end
---
+++ Pods/Target Support Files/Pods-FleapUITests/Pods-FleapUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FleapUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FleapUITests
+@end
